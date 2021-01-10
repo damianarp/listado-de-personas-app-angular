8043a38d15b156c683d43c7e0968d1d48bc95472
Damian Arp <damianarp@gmail.com>
2021-01-10 11:47:13 -0300

    Creación del módulo app-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { PersonasService } from './personas.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+import { PersonasService } from './personas.service';
+import { AppComponent } from './app.component';
 import { PersonaComponent } from './persona/persona.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { LoggingService } from './LoggingService.service';
@@ -15,7 +16,7 @@ import { LoggingService } from './LoggingService.service';
     FormularioComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, AppRoutingModule
   ],
   providers: [LoggingService, PersonasService], // Se debe agregar el proveedor en el componente padre para poder utilizar el servicio en los componentes hijos sin necesidad de crearlo para cada uno de ellos.
   bootstrap: [AppComponent]
